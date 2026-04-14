d35127a32ac457c24e6a23ed0b2dc8c27d1408fc
akshaygite24 <akshaygite2004@gmail.com>
2026-04-14 20:05:10 +0530

    Fixed issues in agents.py and tested agents

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
-from langchain.agents import AgentExecuter, create_react_agent
+from langchain_classic.agents import AgentExecutor, create_react_agent
 from langchain_groq import ChatGroq
-from langchain import hub
+from langsmith import Client
 from tools import calculator, string_length, word_count
 from dotenv import load_dotenv
 
@@ -13,7 +13,8 @@ llm = ChatGroq(
 
 tools = [calculator, string_length, word_count]
 
-prompt = hub.pull("hwchase17/react")
+client = Client()
+prompt = client.pull_prompt("hwchase17/react")
 
 agent = create_react_agent(
     llm=llm,
@@ -22,8 +23,14 @@ agent = create_react_agent(
 )
 
 
-agent_executer = AgentExecuter(
+agent_executer = AgentExecutor(
     agent=agent,
     tools=tools,
     verbose=True
 )
+
+if __name__ == "__main__":
+    response = agent_executer.invoke({
+        "input": "how many words and characters are in i am ai agent?"
+    })
+    print(response["output"])
